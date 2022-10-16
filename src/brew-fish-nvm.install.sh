4b91711a778603b66caeb8624424143da9785e08
Nik Kita <nikkita@Nikitas-MacBook-Pro.local>
2022-10-16 23:45:26 +0300

    brew, nvm, fish(default shell)

--- src/brew-fish-nvm.install.sh
+++ src/brew-fish-nvm.install.sh
@@ -4,60 +4,60 @@ then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || echo """\
 => unable to '/bin/bash -c \"\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\"'\
 """
-    eval "$(/opt/homebrew/bin/brew shellenv)" || echo "=> unable to 'eval \"\$(/opt/homebrew/bin/brew shellenv)\"'"
+    eval "$(/opt/homebrew/bin/brew shellenv)" || echo ">>-----> unable to 'eval \"\$(/opt/homebrew/bin/brew shellenv)\"'"
 else
-    echo "=> brew is already installed"
+    echo ">>-----> brew is already installed"
 fi
 # -------------------------------------------------------------------------------------------------
 fish -v || needFish=true
 if [[ $needFish ]];
 then
-    brew install fish || echo "=> unable to 'brew install fish'"
-    fish -c date || echo "=> unable to 'fish -c date'"
+    brew install fish || echo ">>-----> unable to 'brew install fish'"
+    fish -c date || echo ">>-----> unable to 'fish -c date'"
 else
-    echo "=> fish is already installed"
+    echo ">>-----> fish is already installed"
 fi
 # -------------------------------------------------------------------------------------------------
 nvm -v || needNvm=true
 if [[ $needNvm ]];
 then
-    brew install nvm || echo "=> unable to 'brew install nvm'"
-    mkdir ~/.nvm || echo "=> unable to 'mkdir ~/.nvm'"
-    touch ~/.bash_profile || echo "=> unable to 'touch ~/.bash_profile'"
+    brew install nvm || echo ">>-----> unable to 'brew install nvm'"
+    mkdir ~/.nvm || echo ">>-----> unable to 'mkdir ~/.nvm'"
+    touch ~/.bash_profile || echo ">>-----> unable to 'touch ~/.bash_profile'"
     echo """\
 export PATH="/usr/local/bin:\$PATH"
 export PATH="/opt/homebrew/bin:\$PATH"
 export NVM_DIR=~/.nvm
 source \$(brew --prefix nvm)/nvm.sh\
-""" > ~/.bash_profile || echo "=> unable to ... > ~/.bash_profile"
+""" > ~/.bash_profile || echo ">>-----> unable to ... > ~/.bash_profile"
     cd
-    git clone https://github.com/edc/bass.git || echo "=> unable to 'git clone https://github.com/edc/bass.git'"
-    cd bass || echo "=> unable to 'cd bass'"
-    make install || echo "=> unable to 'make install'"
+    git clone https://github.com/edc/bass.git || echo ">>-----> unable to 'git clone https://github.com/edc/bass.git'"
+    cd bass || echo ">>-----> unable to 'cd bass'"
+    make install || echo ">>-----> unable to 'make install'"
     cd
-    touch ~/.config/fish/functions/nvm.fish || echo "=> unable to 'touch ~/.config/fish/functions/nvm.fish'"
+    touch ~/.config/fish/functions/nvm.fish || echo ">>-----> unable to 'touch ~/.config/fish/functions/nvm.fish'"
     echo """\
 function nvm
 bass source ~/.nvm/nvm.sh --no-use ';' nvm \$argv
 end\
-""" > ~/.config/fish/functions/nvm.fish || echo "=> unable to ... > ~/.config/fish/functions/nvm.fish'"
+""" > ~/.config/fish/functions/nvm.fish || echo ">>-----> unable to ... > ~/.config/fish/functions/nvm.fish'"
 else
-    echo "=> nvm is already installed"
+    echo ">>-----> nvm is already installed"
 fi
 # -------------------------------------------------------------------------------------------------
-iam=$(echo $(whoami))
+touch ~/.zshrc || echo ">>-----> unable to 'touch ~/.zshrc'"
 echo """\
-# Setup fzf
-if [[ ! \"\$PATH\" == */Users/$iam/.fzf/bin* ]]; then
-PATH=\"\${PATH:+\${PATH}:}/Users/$iam/.fzf/bin\"
-fi
-
-# Auto-completion
-[[ \$- == *i* ]] && source \"/Users/$iam/.fzf/shell/completion.bash\" 2> /dev/null
-
-# Key bindings
-source \"/Users/$iam/.fzf/shell/key-bindings.bash\"
-""" > ~/.fzf.bash || echo "=> unable to ... > ~/.fzf.bash"
-echo "[ -f ~/.fzf.bash ] && source ~/.fzf.bash" > ~/.bashrc || echo "=> unable to ... > ~/.bashrc"
-source .bash_profile
+PATH=\"/usr/local/bin:\$PATH\"
+export NVM_DIR=~/.nvm
+source \$(brew --prefix nvm)/nvm.sh
+if [ -f ~/.bash_profile ]; then
+  . ~/.bash_profile
+fi\
+""" > ~/.zshrc || echo ">>-----> unable to ... > ~/.zshrc"
+cat .bash_profile > .bashrc || echo ">>-----> unable to 'cat .bash_profile > .bashrc'"
+sudo chown $(whoami) /etc/shells
+echo "/opt/homebrew/bin/fish" >> /etc/shells
+chsh -s /opt/homebrew/bin/fish
+sudo chown root /etc/shells
+fish
 # -------------------------------------------------------------------------------------------------
--- src/brew-fish-nvm.uninstall.sh
+++ src/brew-fish-nvm.uninstall.sh
@@ -1,15 +1,17 @@
 echo =================================================================================================================
 cd
-brew uninstall fish || echo "=> unable to 'brew uninstall fish'"
-brew uninstall nvm || echo "=> unable to 'brew uninstall nvm'"
-rm -fr /opt/homebrew/etc/fish || echo "=> unable to 'rm -fr /opt/homebrew/etc/fish'"
-rm -fr .config/fish || echo "=> unable to 'rm -fr .config/fish'"
-rm -fr .nvm || echo "=> unable to 'rm -fr .nvm'"
-rm -fr bass || echo "=> unable to 'rm -fr bass'"
-rm .bash_profile || echo "=> unable to 'rm .bash_profile'"
-rm .zprofile || echo "=> unable to 'rm .zprofile'"
-rm .bashrc || echo "=> unable to 'rm .bashrc'"
+brew uninstall fish || echo ">>-----> unable to 'brew uninstall fish'"
+brew uninstall nvm || echo ">>-----> unable to 'brew uninstall nvm'"
+rm -fr /opt/homebrew/etc/fish || echo ">>-----> unable to 'rm -fr /opt/homebrew/etc/fish'"
+rm -fr ~/.config/fish || echo ">>-----> unable to 'rm -fr ~/.config/fish'"
+rm -fr ~/.nvm || echo ">>-----> unable to 'rm -fr ~/.nvm'"
+rm -fr ~/bass || echo ">>-----> unable to 'rm -fr ~/bass'"
+rm ~/.bash_profile || echo ">>-----> unable to 'rm ~/.bash_profile'"
+rm ~/.zprofile || echo ">>-----> unable to 'rm ~/.zprofile'"
+rm ~/.bashrc || echo ">>-----> unable to 'rm ~/.bashrc'"
+rm ~/.fzf.bash || echo ">>-----> unable to 'rm ~/.fzf.bash'"
+rm ~/.zshrc || echo ">>-----> unable to 'rm ~/.zshrc'"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)" || echo """\
 => unable to '/bin/bash -c \"\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)'\""""
-echo "=> fish-nvm-brew uninstallation completed!"
+echo ">>-----> fish-nvm-brew uninstallation completed!"
 echo =================================================================================================================
